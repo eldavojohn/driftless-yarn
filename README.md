1aaa66e38236bbee20cb279fbc748cf7f288dca6
eldavojohn <eldavojohn@gmail.com>
2021-10-26 22:13:08 -0500

    docs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # driftless-yarn
 A simple tool to consume a project's yarn.lock and ensure certain dependencies are singular.
+
+# Sample usage (more to come)
+```console
+yarn add -D driftless-yarn
+node ./node_modules/driftless-yarn/src/index.js -p https://github.com/eldavojohn/driftless-yarn/blob/main/public/failingList.txt
+```
+
+This will use the project's `yarn.lock` file and compare it against failingList from this repo.  
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
 var https_1 = __importDefault(require("https"));
@@ -11,58 +9,54 @@ var https_1 = __importDefault(require("https"));
 var lockfile_1 = require("@yarnpkg/lockfile");
 var commander_1 = require("commander");
 var program = new commander_1.Command();
-program.requiredOption(
-  "-p, --package-list <file or url>",
-  "a relative file path or https URL to the list of packages"
-);
+program
+    .requiredOption("-p, --package-list <file or url>", "a relative file path or https URL to the list of packages")
+    .option("-y, --yarn-location <file location of yarn.lock>", "a relative file path to the yarn.lock in question");
 program.parse();
 var options = program.opts();
-var file = fs_1.default.readFileSync("yarn.lock", "utf8");
+var file = fs_1.default.readFileSync(options.yarnLocation || "yarn.lock", "utf8");
 var json = (0, lockfile_1.parse)(file);
 var artifactRegistry = {};
 var offenders = [];
 Object.keys(json.object).forEach(function (key) {
-  var implodedEntry = json.object[key];
-  var name = key[0] === "@" ? "@" + key.split("@")[1] : key.split("@")[0];
-  var artifact = implodedEntry.resolved;
-  if (
-    artifactRegistry[name] &&
-    artifactRegistry[name].indexOf(artifact) === -1
-  ) {
-    artifactRegistry[name].push(artifact);
-  } else if (!artifactRegistry[name]) {
-    artifactRegistry[name] = [artifact];
-  }
-  if (artifactRegistry[name].length > 1 && offenders.indexOf(name) === -1) {
-    offenders.push(name);
-  }
+    var implodedEntry = json.object[key];
+    var name = key[0] === "@" ? "@" + key.split("@")[1] : key.split("@")[0];
+    var artifact = implodedEntry.resolved;
+    if (artifactRegistry[name] &&
+        artifactRegistry[name].indexOf(artifact) === -1) {
+        artifactRegistry[name].push(artifact);
+    }
+    else if (!artifactRegistry[name]) {
+        artifactRegistry[name] = [artifact];
+    }
+    if (artifactRegistry[name].length > 1 && offenders.indexOf(name) === -1) {
+        offenders.push(name);
+    }
 });
 function compareLists(packageList, badList) {
-  packageList.split("\n").forEach(function (pkg) {
-    if (badList.indexOf(pkg) !== -1) {
-      process.exit(1);
-    }
-  });
-  process.exit(0);
+    packageList.split("\n").forEach(function (pkg) {
+        if (badList.indexOf(pkg) !== -1) {
+            process.exit(1);
+        }
+    });
+    process.exit(0);
 }
 if (options.packageList.indexOf("https") === 0) {
-  https_1.default
-    .get(options.packageList, function (res) {
-      var data = [];
-      res.on("data", function (chunk) {
-        data.push(chunk);
-      });
-      res.on("end", function () {
-        compareLists(Buffer.concat(data).toString(), offenders);
-      });
+    https_1.default
+        .get(options.packageList, function (res) {
+        var data = [];
+        res.on("data", function (chunk) {
+            data.push(chunk);
+        });
+        res.on("end", function () {
+            compareLists(Buffer.concat(data).toString(), offenders);
+        });
     })
-    .on("error", function (err) {
-      console.log("Error: ", err.message);
-      process.exit(1);
+        .on("error", function (err) {
+        console.log("Error: ", err.message);
+        process.exit(1);
     });
-} else {
-  compareLists(
-    fs_1.default.readFileSync(options.packageList, "utf8"),
-    offenders
-  );
+}
+else {
+    compareLists(fs_1.default.readFileSync(options.packageList, "utf8"), offenders);
 }
